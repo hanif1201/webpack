8b965054147099b7b1f292218ff60f23344ffe09
Hanif1201 <adeyemihanif@gmail.com>
2023-10-01 01:04:40 +0100

    webpack

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { webpack } from "webpack";
+import randomjoke from "./randomjoke";
+
+console.log("Be funny");
+
+q: I am tired, what can I do
+a: Take a nap
+
+q: package to use live server on webpack
+
+
+
+
---
+++ src/randomjoke.js
@@ -0,0 +1,11 @@
+function randomjoke() {
+  return "You are not funny";
+}
+
+export default randomjoke;
+
+//what to put in gitignore
+
+//node_modules  - this is where all the packages are installed  - you don't want to push this to github because it's too big  - you can always reinstall the packages by running npm install  - you can also run npm install to install all the packages in the package.json file   - you can also run npm install --save to install a package and add it to the package.json file
+
+//how to
